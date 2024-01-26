ea43994f9653ae97a119a806f63997f4431574d5
polyprogrammist <vad.e.volodin@gmail.com>
2024-01-26 18:23:06 -0500

    Change TimelineStatus

--- src/proposal/timeline.rs
+++ src/proposal/timeline.rs
@@ -6,10 +6,38 @@ use near_sdk::serde::{Serialize, Deserialize};
 #[borsh(crate = "near_sdk::borsh")]
 pub enum TimelineStatus {
     Draft,
-    Review { sponsor_requested_review: bool, reviewer_completed_attestation: bool },
-    Approved,
-    Rejected,
-    ApprovedConditionally,
-    PaymentProcessing { kyc_verified: bool, test_transaction_sent: bool, request_for_trustees_created: bool },
-    Funded
+    Review(ReviewStatus),
+    Approved(ReviewStatus),
+    Rejected(ReviewStatus),
+    ApprovedConditionally(ReviewStatus),
+    PaymentProcessing(PaymentProcessingStatus),
+    Funded(FundedStatus),
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+#[borsh(crate = "near_sdk::borsh")]
+pub struct ReviewStatus {
+    sponsor_requested_review: bool,
+    reviewer_completed_attestation: bool
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+#[borsh(crate = "near_sdk::borsh")]
+pub struct PaymentProcessingStatus {
+    #[serde(flatten)]
+    review_status: ReviewStatus,
+    kyc_verified: bool,
+    test_transaction_sent: bool,
+    request_for_trustees_created: bool,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+#[borsh(crate = "near_sdk::borsh")]
+pub struct FundedStatus {
+    #[serde(flatten)]
+    payment_processing_status: PaymentProcessingStatus,
+    trustees_released_payment: bool,
 }
